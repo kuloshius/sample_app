8d6812d19e93d7326b6e896572f8b835610b6fe9
Kuloshius <tomas@ebox.lt>
2011-01-18 19:07:50 +0000

    Added feature to reset user password on create failure

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,11 +11,13 @@ describe "Users" do
 					visit signup_path
 					fill_in "Name",	:with => ""
 					fill_in "Email",	:with => ""
-					fill_in "Password",	:with => ""
-					fill_in "Confirmation",	:with => ""
+					fill_in "Password",	:with => "foobar"
+					fill_in "Confirmation",	:with => "foobar"
 					click_button
 					response.should render_template('users/new')
 					response.should have_selector("div#error_explanation")
+					response.should_not have_selector("input#user_password", :value => "foobar")
+					response.should_not have_selector("input#user_password_confirmation", :value => "foobar")
 				end.should_not change(User, :count)
 			end
 		end
